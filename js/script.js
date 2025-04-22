67df36c233bb8dc1c5eceb71b30075a6803f3606
mkmahmud <mahmudulhasanmk434@gmail.com>
2025-04-22 12:22:55 +0600

    DONE Banner Section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+ // Counter Animation
+ const counters = document.querySelectorAll('.counter');
+ counters.forEach(counter => {
+   const target = parseInt(counter.getAttribute('data-target'));
+   const increment = target / 200;
+   
+   function updateCount() {
+     const count = +counter.innerText;
+     if(count < target) {
+       counter.innerText = Math.ceil(count + increment);
+       setTimeout(updateCount, 10);
+     } else {
+       counter.innerText = target;
+     }
+   }
+   
+   updateCount();
+ });
 
  // Mobile Menu Toggle
  const mobileMenuButton = document.getElementById('mobile-menu-button');
